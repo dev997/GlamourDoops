80c7361f269bc3a3943269016e5be1dd33a29e41
dev997 <mrdev997@gmail.com>
2026-09-24 09:42:59 -0500

    remove image reference

--- GlamourDoops/Windows/MainWindow.cs
+++ GlamourDoops/Windows/MainWindow.cs
@@ -11,13 +11,12 @@ namespace SamplePlugin.Windows;
 
 public class MainWindow : Window, IDisposable
 {
-    private readonly string goatImagePath;
     private readonly Plugin plugin;
 
     // We give this window a hidden ID using ##.
     // The user will see "My Amazing Window" as window title,
     // but for ImGui the ID is "My Amazing Window##With a hidden ID"
-    public MainWindow(Plugin plugin, string goatImagePath)
+    public MainWindow(Plugin plugin)
         : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
         SizeConstraints = new WindowSizeConstraints
